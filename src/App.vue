61920407e8e0e51841f8c8e9ac2bee5518223d5c
yordisev <yordys12@gmail.com>
2021-09-03 15:20:57 -0500

    utilizar unas funciones en varias vistas y compone

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="nav">
     <router-link to="/">Inicio</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/titulo">Titulo</router-link>
+    <router-link to="/titulo">Titulo</router-link> |
+    <router-link to="/contador">Contador</router-link> |
   </div>
   <router-view/>
 </template>
---
+++ src/components/Btn.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <button @click="accionHijo">{{textoBoton}}</button> -->
+<button @click="$emit('accion')">{{textoBoton}}</button>
+
+</template>
+
+<script>
+export default {
+    props:['textoBoton'],
+    setup(props, context){
+        const accionHijo = () =>{
+            context.emit('accion')
+        }
+        return {accionHijo}
+    }
+}
+</script>
+
+<style>
+
+</style>
