0d0faf9548a623a7d3fe1278a0889a606cb74d6c
yordisev <yordys12@gmail.com>
2021-09-04 13:08:36 -0500

    prueba

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Bienvenido Your Vue App"/>
 <h1
 :style="{'color': color}"
 >contador: {{contador}}</h1>
