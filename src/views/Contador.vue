c7000fd55ae01a1ad3efd1efa0da461f34b74f82
yordisev <yordys12@gmail.com>
2021-09-04 15:15:43 -0500

    contador

--- src/views/Contador.vue
+++ src/views/Contador.vue
@@ -1,12 +1,18 @@
 <template>
   <h1>Contador: {{contador}}</h1>
+  <Btn :textoBoton="'Aumentar'" @accion="aumentar"/>
+  <Btn :textoBoton="'Disminuir'" @accion="disminuir"/>
 </template>
 
 <script>
+import Btn from '../components/Btn.vue'
 import {useContador} from '../hooks/useContador'
 export default {
+  components:{
+Btn
+  },
     setup(){
-        return {...useContador}
+        return {...useContador()}
     }
 }
 </script>
